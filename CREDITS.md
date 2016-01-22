5ec153b311c7230465eec6a96571d93209eed546
MJ Rossetti <mrossetti@codeforamerica.org>
2016-01-22 12:28:25 -0800

    handling malformatted csv values

--- CREDITS.md
+++ CREDITS.md
@@ -2,3 +2,5 @@
 
   + https://github.com/codeforamerica/courtbot
   + https://github.com/kuanb/atl_zoink
+  + http://stackoverflow.com/questions/9864064/csv-read-illegal-quoting-in-line-x
+  + http://donsnotes.com/tech/charsets/ascii.html
--- download.rb
+++ download.rb
@@ -12,7 +12,7 @@ end
 
 # @param [Date] day
 def csv_url(day)
-  url_date = day.strftime("%d%m%Y")
+  url_date = day.strftime("%d%m%Y") # "20012014"
   "http://courtview.atlantaga.gov/courtcalendars/court_online_calendar/codeamerica.#{url_date}.csv"
 end
 
@@ -27,20 +27,25 @@ def csv_headers
     "citation_id","citation_violation_id","citation_violation_description","citation_payable"]
 end
 
+def csv_parse_options
+  {
+    :col_sep => "|", # should parse pipe-delimited data
+    :quote_char => "\x00" # should parse unexpected values like "SMITH, DANT"E"
+  }
+end
+
 days.each do |d|
+  next if File.exist?(csv_file_path(d))
+
   response = HTTParty.get(csv_url(d))
   puts "#{d} - #{response.code}"
   next if response.code == 404
 
-  rows = CSV.parse(response.body, {:col_sep => "|"}) # ["07-MAY-14", "FORRESTER, KIM", "17TH & INMAN", "6A", "08:00:00 AM", "4678114", "40-6-72", "FAIL TO STOP AT STOP LINE", "1"]
+  rows = CSV.parse(response.body, csv_parse_options) # ["07-MAY-14", "FORRESTER, KIM", "17TH & INMAN", "6A", "08:00:00 AM", "4678114", "40-6-72", "FAIL TO STOP AT STOP LINE", "1"]
 
   CSV.open(csv_file_path(d), "w", :write_headers=> true, :headers => csv_headers) do |csv|
     rows.each do |row|
-      begin
-        csv << row
-      rescue
-        binding.pry
-      end
+      csv << row
     end
   end
 end
