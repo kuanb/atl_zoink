13cc9185b7cc405c94180eaa54a0bfdfdb8aa25a
Kuan Butts <kuanbutts@yahoo.com>
2016-01-22 12:50:28 -0800

    searchall restructure

--- searchall.py
+++ searchall.py
@@ -10,10 +10,8 @@ def daterange(start_date, end_date):
 
 start_date = date(2014, 1, 1)
 end_date = date(2016, 1, 1)
-
-
-start_date = date(2015, 1, 1)
-end_date = date(2016, 1, 1)
+start_date = date(2014, 10, 1)
+end_date = date(2016, 10, 5)
 
 
 # open csvs then clean and prep to write to them as we get results
@@ -26,34 +24,38 @@ f.close()
 f = open('alldata.csv', "w+")
 f.close()
 
-empty = open('empty.csv','a')
-hasdata = open('hasdata.csv','a')
-alldata = open('alldata.csv','a')
 
 
-
-for single_date in daterange(start_date, end_date):
+for single_date in daterange(start_date, end_date):	
 	curr_date = single_date.strftime("%m%d%Y")
 	new_url = url_base + curr_date + ".csv"
+
 	try:
 		response = urllib2.urlopen(new_url)
 		html = response.read()
-
 		reader = csv.reader(html)
-		for r in reader:
-			alldata.write(curr_date)
 
+		# alldata = csv.reader(open('alldata.csv'),  delimiter=',')
+		for r in reader:
+			alldata = open('alldata.csv','a')
+			alldata.write(curr_date)
+			alldata.write("\n")
+			alldata.close()
+
+		hasdata = open('hasdata.csv','a')
 		hasdata.write(curr_date)
+		hasdata.write("\n")
+		hasdata.close()
 
 		print "Got a hit on " + curr_date
 
 	except urllib2.HTTPError, e:
-		print "None for " + curr_date
+		empty = open('empty.csv','a')
 		empty.write(curr_date)
+		empty.write("\n")
+		empty.close()
 
-	empty.write("\n")
-	hasdata.write("\n")
-	alldata.write("\n")
+		print "None for " + curr_date
 
 
 
